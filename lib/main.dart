a3b8c4eac7d1d3eb9bb43a3753f47153d2ca7108
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-08-19 14:33:14 +0900

    29 install provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:cab_rider/dataprovider/appdata.dart';
 import 'package:cab_rider/screens/loginpage.dart';
 import 'package:cab_rider/screens/mainpage.dart';
 import 'package:cab_rider/screens/registrationpage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,18 +17,21 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Brand-Regular',
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => AppData(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          fontFamily: 'Brand-Regular',
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: MainPage.id,
+        routes: {
+          RegistrationPage.id: (content) => RegistrationPage(),
+          LoginPage.id: (content) => LoginPage(),
+          MainPage.id: (content) => MainPage(),
+        },
       ),
-      initialRoute: MainPage.id,
-      routes: {
-        RegistrationPage.id: (content) => RegistrationPage(),
-        LoginPage.id: (content) => LoginPage(),
-        MainPage.id: (content) => MainPage(),
-      },
     );
   }
 }
