30f938ba6787234ab2e729d1ca594ae80f613537
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-08-20 20:17:02 +0900

    39 選択した場所の詳細を取得

--- lib/dataprovider/appdata.dart
+++ lib/dataprovider/appdata.dart
@@ -4,8 +4,15 @@ import 'package:flutter/cupertino.dart';
 class AppData extends ChangeNotifier {
   Address pickupAddress;
 
+  Address destinationAddress;
+
   void updatePickupAddress(Address pickup) {
     pickupAddress = pickup;
+    notifyListeners();
+  }
+
+  void updateDestinationAddress(Address destination) {
+    destinationAddress = destination;
     notifyListeners();
   }
 }
